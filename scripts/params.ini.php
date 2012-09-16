2d77df07cf4a94a208c7812d872fbb3750fd2ac0
nikorova <nick@korovasoft.com>
2012-09-16 00:19:23 -0500

    change db params to relfect dev db

--- scripts/params.ini.php
+++ scripts/params.ini.php
@@ -1,8 +1,8 @@
 <?php
 
 /* params */
-$mysqlHost 		= 'localhost';
-$mysqlDB 		= 'coupCal';
-$mysqlUser 		= 'coupCal';
-$mysqlPass		= 'coupCalPass';
+$mysqlHost				= 'tunnel.pagodabox.com:3306';
+$mysqlDB				= 'coup-db-dev';
+$mysqlUser				= 'kimberley';
+$mysqlPass				= 'IY9Z1wnr';
 $imageUploadDir 		= '/var/www/localhost/the_coup/uploads';
