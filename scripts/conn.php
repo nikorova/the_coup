30b69f4441aae2e00a016d812e02b2557b6767f6
root <root@snorf.(none)>
2012-10-02 00:36:54 -0500

    reinstate conn.php

---
+++ scripts/conn.php
@@ -0,0 +1,12 @@
+<?php
+require_once('params.ini.php');
+
+/***
+ * create new db connection
+ */
+try {
+	$dbh = new PDO("mysql:host=$mysqlHost;dbname=$mysqlDB", $mysqlUser, $mysqlPass);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	echo "db connection failed: " . $e->getMessage();	
+}
