8eaf7b9f2c9a89b5332e95619a70376272779397
nikorova <oneofy@gmail.com>
2012-10-02 01:20:44 -0500

    tweak params formatting

--- scripts/params.ini.php
+++ scripts/params.ini.php
@@ -1,9 +1,9 @@
 <?php
 
 /* params */
-$mysqlHost				= 'localhost';
-$mysqlDB				= 'coupCal';
-$mysqlEventsTable		= 'events';
-$mysqlUser				= 'coupCal';
-$mysqlPass				= 'coupCalPass';
-$imageUploadDir 		= '/var/www/localhost/the_coup/uploads';
+$mysqlHost		= 'localhost';
+$mysqlDB		= 'coupCal';
+$mysqlEventsTable	= 'events';
+$mysqlUser		= 'coupCal';
+$mysqlPass		= 'coupCalPass';
+$imageUploadDir 	= '/var/www/localhost/the_coup/uploads';
